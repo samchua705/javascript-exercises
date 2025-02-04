69527de709f820e12b2fda48d2645f8503afcc75
Sam Sam Chua <karenchua705@gmail.com>
2025-02-04 21:51:23 +0800

    edit js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(arg) {
+    let array = [];
+    for (let i = 0; i < arg.length; i++) {
+        array.push(arg[i]);
+    }
+    return array.reverse().join('');
 };
+
+console.log(reverseString("hello")); // Output: "olleh"
 
 // Do not edit below this line
 module.exports = reverseString;
